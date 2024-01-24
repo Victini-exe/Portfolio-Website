1031dab1a51840e9a21683c9e030536345e0110e
Victini-exe <shyaalternate@gmail.com>
2024-01-25 00:26:02 +0530

    first commit

---
+++ js/validator.js
@@ -0,0 +1,50 @@
+function checkcontact(theForm) {
+    var why = "";
+	why += isEmptyName(theForm.name.value);
+	why += checkemail(theForm.email.value);
+	why += isEmptyMessage(theForm.message.value);
+	
+    if (why != "") {
+       alert(why);
+       return false;
+    }
+return true;
+}
+
+
+function isEmptyName(strng) {
+	var error = "";
+	  if (strng.length == 0) {
+		 error = "Please Enter your name! \n"
+	  }
+	return error;	  
+}
+
+function isEmptyMessage(strng) {
+	var error = "";
+	  if (strng.length == 0) {
+		 error = "Please Enter your message! \n"
+	  }
+	return error;	  
+}
+
+
+
+function checkemail (strng) {
+	var error="";
+	if (strng == "") {
+	   error = "Enter your email address?.\n";
+	}
+	
+		var emailFilter=/^.+@.+\..{2,3}$/;
+		if (!(emailFilter.test(strng))) { 
+		   error = "Please enter a valid email address.\n";
+		}
+		else {
+		   var illegalChars= /[\(\)\<\>\,\;\:\\\"\[\]]/
+			 if (strng.match(illegalChars)) {
+			  error = "The email address contains illegal characters.\n";
+		   }
+		}
+	return error;    
+}
